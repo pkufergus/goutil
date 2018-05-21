1c71dabf99df4e08e60a672fbb37f9094a2bb8b0
pkufergus <pkufergus@qq.com>
2018-05-21 17:09:42 +0800

    time cost 2
    Signed-off-by: pkufergus <pkufergus@qq.com>

--- util.go
+++ util.go
@@ -6,33 +6,38 @@ import (
 	"fmt"
 )
 
-func TimeCost(logfunc interface{}) func() {
+func TimeCost(logfunc interface{}, logid uint32) func() {
 	start := time.Now()
 	pc,file,line,_ := runtime.Caller(1)
 	switch logfunc.(type) {
 	case func(f interface{}, v ...interface{}):
 		f := logfunc.(func(f interface{}, v ...interface{}))
-		f("[%s:%d %s]enter ", file, line, runtime.FuncForPC(pc).Name())
+		f("[%s:%d %s][logid:%d]enter ", file, line, runtime.FuncForPC(pc).Name(), logid)
 	case func(s string, a ...interface{}):
 		f := logfunc.(func(s string, a ...interface{}))
-		f("[%s:%d %s]enter ", file, line, runtime.FuncForPC(pc).Name())
+		f("[%s:%d %s][logid:%d]enter ", file, line, runtime.FuncForPC(pc).Name(), logid)
 	default:
 		f := fmt.Printf
-		f("[%s:%d %s]enter\n", file, line, runtime.FuncForPC(pc).Name())
+		f("[%s:%d %s][logid:%d]enter\n", file, line, runtime.FuncForPC(pc).Name(), logid)
 	}
 	return func() {
 		pc,file,line,_ := runtime.Caller(1)
 		switch logfunc.(type) {
 		case func(f interface{}, v ...interface{}):
 			f := logfunc.(func(f interface{}, v ...interface{}))
-			f("[%s:%d %s]exit cost:%s", file, line, runtime.FuncForPC(pc).Name(), time.Since(start))
+			f("[%s:%d %s][logid:%d]exit cost:%s", file, line, runtime.FuncForPC(pc).Name(), logid, time.Since(start))
 		case func(s string, a ...interface{}):
 			f := logfunc.(func(s string, a ...interface{}))
-			f("[%s:%d %s]exit cost:%s", file, line, runtime.FuncForPC(pc).Name(), time.Since(start))
+			f("[%s:%d %s][logid:%d]exit cost:%s", file, line, runtime.FuncForPC(pc).Name(), logid, time.Since(start))
 		default:
 			f := fmt.Printf
-			f("[%s:%d %s]exit cost:%s\n", file, line, runtime.FuncForPC(pc).Name(), time.Since(start))
+			f("[%s:%d %s][logid:%d]exit cost:%s\n", file, line, runtime.FuncForPC(pc).Name(), logid, time.Since(start))
 		}
 	}
+}
+
+func GetDateFromTimestamp(sec int64) {
+	tm := time.Unix(sec, 0)
+	fmt.Println(tm.Format("20060102"))
 }
 
--- util_test.go
+++ util_test.go
@@ -8,8 +8,18 @@ import (
 
 func TestTimeCost(t *testing.T) {
 	fmt.Println("start test")
-	defer TimeCost(fmt.Println)()
+	defer TimeCost(fmt.Println, 12321)()
 	time.Sleep(time.Second)
+}
+
+func TestGetDateFromTimestamp(t *testing.T) {
+	GetDateFromTimestamp(1526449519)
+
+	the_time := time.Date(2018, 3, 21, 0, 0, 0, 0, time.Local)
+	fmt.Println(the_time.Format("20060102"))
+	d, _ := time.ParseDuration("2400h")
+	new_time := the_time.Add(d)
+	fmt.Println(new_time.Format("20060102"))
 }
 
 
